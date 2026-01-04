c4f85c0e67e0531d8ca1b0a737de4695acd22dcf
Vaibhav Mali <vaibhavmali17@gmail.com>
2026-01-04 22:10:18 +0530

    I am create basic UI Using html , css , javascript

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Day 1,2,3
 const button = document.getElementById("actionBtn");
 const text = document.getElementById("status-text");
 
@@ -6,31 +5,3 @@ button.addEventListener("click", () => {
   text.innerText =
     "Day 3 Completed ✅ JavaScript + GitHub progress updated!";
 });
-
-// Day 4
-function showMessage() {
-  document.getElementById("message").innerText =
-    "🔥 JavaScript is working! You are becoming a developer.";
-}
-//  Day 5
-
-function loadSkills() {
-  const skills = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "Git & GitHub",
-    "Basic React (Learning)",
-    "Problem Solving"
-  ];
-
-  const list = document.getElementById("skillsList");
-  list.innerHTML = ""; // clear old content
-
-  skills.forEach(skill => {
-    const li = document.createElement("li");
-    li.innerText = skill;
-    list.appendChild(li);
-  });
-}
-
