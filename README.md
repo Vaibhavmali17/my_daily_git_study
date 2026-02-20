7df183e6fcaca461264641d817899a895dd1925c
Vaibhav Mali <vaibhavmali17@gmail.com>
2026-02-20 19:46:40 +0530

    Added two new html pages and modify css styling

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # SkillSprint – Developer Learning Tracker
-  From today, I am starting a basic full-stack project to track my daily learning and growth as a software developer.
-  navbar links working, anchor navigation done. 
+
+
+  I am starting a basic full-stack project to track my daily learning and growth as a software developer.
+  navbar links working, anchor navigation done.
+  Login Page Done.
+(Added two new html pages and modify css styling)
--- script.js
+++ script.js
@@ -5,3 +5,17 @@ button.addEventListener("click", () => {
   text.innerText =
     "Day 3 Completed ✅ JavaScript + GitHub progress updated!";
 });
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  if (email && password) {
+    window.location.href = "dashboard.html";
+  } else {
+    alert("Please enter email & password");
+  }
+}
+
+function logout() {
+  window.location.href = "index.html";
+}
